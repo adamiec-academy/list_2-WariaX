15d84ee2f5274f8d8bac602949f2b1497f724d69
WariaX <115689237+WariaX@users.noreply.github.com>
2022-11-01 00:32:17 +0100

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -2,14 +2,16 @@ def remove_parentheses(text):
     result = ""
     is_inside = False
 
-    for x in text:
-        if x == "(":            
+    for x in range(len(text)):
+        if text[x] == "(":            
             is_inside = True
 
-        elif x == ")":            
+        elif text[x] == " " and text[x-1] == ")":    
             is_inside = False
 
         elif is_inside == False:
-            result += x            
+            result += text[x]            
 
     return result.replace ("  "," ")
+    
+print(remove_parentheses('text')) 
