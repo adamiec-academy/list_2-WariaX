956cf85b680206d60def6223cfffb39dbbf8a7ab
WariaX <115689237+WariaX@users.noreply.github.com>
2022-11-09 15:33:36 +0100

    Update task_5.py

--- task_5.py
+++ task_5.py
@@ -1,6 +1,56 @@
-def cipher(text, shift):
-    return text
+def cipher(text, shift):    
+    result = ""   
+    
+    for letter in range(len(text)):         
+        
+        if ord(text[letter]) == 32:
+                result += " "      
+        elif text[letter].isupper():
+            
 
+            if ord(text[letter]) > 90 - shift:
+                result += chr(ord(text[letter]) + shift - 26)    
 
-def decipher(text, shift):
-    return text
+            else:
+                result += chr(ord(text[letter]) + shift)
+   
+        else:
+                      
+
+            if ord(text[letter]) > 122 - shift:
+                result += chr(ord(text[letter]) + shift - 26)    
+
+            else:
+                result += chr(ord(text[letter]) + shift)
+        
+    
+    return result
+    
+
+def decipher(text, shift):    
+    result = ""   
+    
+    for letter in range(len(text)):
+               
+
+        if ord(text[letter]) == 32:
+                result += " "      
+        elif text[letter].isupper():            
+
+            if ord(text[letter]) - shift < 65:
+                result += chr(ord(text[letter]) - shift + 26)    
+
+            else:
+                result += chr(ord(text[letter]) - shift)
+   
+        else:
+                      
+
+            if ord(text[letter]) - shift < 97 :
+                result += chr(ord(text[letter]) - shift + 26)    
+
+            else:
+                result += chr(ord(text[letter]) - shift)
+        
+    
+    return result
